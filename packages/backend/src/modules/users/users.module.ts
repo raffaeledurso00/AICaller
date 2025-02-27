5474ad53587a4e394f3d48bebabf31f5669aa9be
raffaeledurso00 <raffaeledurso00@yahoo.com>
2025-02-27 10:07:56 +0100

    updated users.module to include the users operations handling service

--- packages/backend/src/modules/users/users.module.ts
+++ packages/backend/src/modules/users/users.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { UsersService } from './services/users.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [],
-  providers: [],
-  exports: [MongooseModule],
+  providers: [UsersService],
+  exports: [MongooseModule, UsersService],
 })
 export class UsersModule {}
