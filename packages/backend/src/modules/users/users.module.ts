26952eb975628d2d901a407614d28d4c0d87a003
raffaeledurso00 <raffaeledurso00@yahoo.com>
2025-02-27 10:00:00 +0100

    updated users.module to regiser users schemas

--- packages/backend/src/modules/users/users.module.ts
+++ packages/backend/src/modules/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [],
   providers: [],
-  exports: [],
+  exports: [MongooseModule],
 })
 export class UsersModule {}
