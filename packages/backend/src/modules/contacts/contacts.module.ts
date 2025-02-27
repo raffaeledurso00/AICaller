51e69c4880442833a04e55415f0d6f37bb3d90c0
raffaeledurso00 <raffaeledurso00@yahoo.com>
2025-02-27 10:01:38 +0100

    updated contacts.module to register contacts schema

--- packages/backend/src/modules/contacts/contacts.module.ts
+++ packages/backend/src/modules/contacts/contacts.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Contact, ContactSchema } from './schemas/contact.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([{ name: Contact.name, schema: ContactSchema }]),
+  ],
   controllers: [],
   providers: [],
-  exports: [],
+  exports: [MongooseModule],
 })
 export class ContactsModule {}
