a8ee19276a228d4a44b1f8005f57aed8ecc0dd46
raffaeledurso00 <raffaeledurso00@yahoo.com>
2025-02-27 10:10:16 +0100

    updated users.module to include the controller

--- packages/backend/src/modules/users/users.module.ts
+++ packages/backend/src/modules/users/users.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 import { UsersService } from './services/users.service';
+import { UsersController } from './controllers/users.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  controllers: [],
+  controllers: [UsersController],
   providers: [UsersService],
   exports: [MongooseModule, UsersService],
 })
