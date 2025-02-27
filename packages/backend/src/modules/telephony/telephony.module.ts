e88d12714ac802c4d8286195372f75085a7ff343
raffaeledurso00 <raffaeledurso00@yahoo.com>
2025-02-27 10:02:19 +0100

    updated telephony.module to register calls schema

--- packages/backend/src/modules/telephony/telephony.module.ts
+++ packages/backend/src/modules/telephony/telephony.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Call, CallSchema } from './schemas/call.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([{ name: Call.name, schema: CallSchema }]),
+  ],
   controllers: [],
   providers: [],
-  exports: [],
+  exports: [MongooseModule],
 })
 export class TelephonyModule {}
