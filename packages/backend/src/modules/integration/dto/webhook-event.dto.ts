c8200accb6190bb2efe8d993e9fa6550183a5fb6
raffaeledurso00 <raffaeledurso00@yahoo.com>
2025-02-28 09:31:02 +0100

    created the webhook event DTO

---
+++ packages/backend/src/modules/integration/dto/webhook-event.dto.ts
@@ -0,0 +1,20 @@
+import { IsEnum, IsNotEmpty, IsObject } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { WebhookEventType } from '../enums/webhook-event-type.enum';
+
+export class WebhookEventDto {
+  @ApiProperty({
+    enum: WebhookEventType,
+    description: 'The type of event that triggered the webhook',
+  })
+  @IsEnum(WebhookEventType)
+  @IsNotEmpty()
+  eventType: WebhookEventType;
+
+  @ApiProperty({
+    description: 'The data payload for the webhook event',
+    type: 'object',
+  })
+  @IsObject()
+  data: Record<string, any>;
+}
